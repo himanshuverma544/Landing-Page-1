d82f23ec64fef18d869139c65fd34e4c27356e1d
Himanshu Verma <himanshuverma544@gmail.com>
2024-06-26 10:50:15 +0530

    add: - main component - app component

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css';
+
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+import About from './components/about/main-components/About';
+import OurServices from './components/our-services/main-components/OurServices';
+import Teamwork from './components/teamwork/main-components/Teamwork';
+import Testimonials from './components/testimonials/main-components/Testimonials';
+import HaveProject from './components/have-project/main-components/HaveProject';
+
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <main>
+        <About/>
+        <OurServices/>
+        <Teamwork/>
+        <Testimonials/>
+        <HaveProject/>
+      </main>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
