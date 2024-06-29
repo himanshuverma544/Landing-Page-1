9b9e9782f5e2f087bbd01980d51c6d81967f070e
Himanshu Verma <himanshuverma544@gmail.com>
2024-06-29 12:38:05 +0530

    upd: made the component responsive

--- src/components/teamwork/sub-components/Teammate.jsx
+++ src/components/teamwork/sub-components/Teammate.jsx
@@ -1,15 +1,15 @@
 export default function Teammate({ className, name, designation, avatar }) {
   return (
-    <div className={`teammate ${className} w-1/3 flex flex-col items-center rounded-[50%] bg-[#091314] text-white`}>
+    <div className={`teammate ${className} w-full flex flex-col items-center rounded-[50%] bg-[#091314] text-white md:w-[31vw] md:h-[31vw]`}>
       <div className="name font-bold text-xs mt-8">
         {name}
       </div>
       <div className="designation uppercase font-bold">
         {designation}
       </div>
-      <div className="avatar w-11/12">
-        <img className="rounded-[50%]" src={avatar} alt={`${name} Avatar`}/>
+      <div className="avatar w-[90%] md:w-[27vw] md:h-[30vw] xl:mt-10 2xl:mt-16">
+        <img className="rounded-[50%] w-full h-full object-cover" src={avatar} alt={`${name} Avatar`}/>
       </div>
     </div>
-  )
+  );
 }
