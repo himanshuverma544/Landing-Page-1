143b5e177c850250f0bdc06c7ba688b4c5ca767f
Himanshu Verma <himanshuverma544@gmail.com>
2024-06-29 12:38:21 +0530

    upd: made the component responsive

--- src/components/teamwork/sub-components/Brand.jsx
+++ src/components/teamwork/sub-components/Brand.jsx
@@ -1,6 +1,6 @@
 export default function Brand({ className = '', icon }) {
   return (
-    <div className={`brand ${className} w-[120px] h-[120px] flex justify-center items-center rounded-[50%] bg-[#091314]`}>
+    <div className={`brand ${className} w-[100px] h-[100px] md:w-[10%] md:h-[10%] flex justify-center items-center rounded-[50%] bg-[#091314]`}>
       {icon}
     </div>
   );
