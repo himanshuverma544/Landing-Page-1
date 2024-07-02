aaff719d716117bf3e39364e7cb287a022c80bc1
Himanshu Verma <himanshuverma544@gmail.com>
2024-07-02 11:03:41 +0530

    upd: - added arrows - added functionality to make arrows disable at beginning and end - refactored code

--- src/components/testimonials/main-components/Testimonials.jsx
+++ src/components/testimonials/main-components/Testimonials.jsx
@@ -1,15 +1,21 @@
+import React from "react";
+
+import { useMediaQuery } from '@react-hook/media-query';
+
 import Carousel from 'react-multi-carousel';
 
 import Avatar from "../sub-components/Avatar";
 
-import Client1 from "../../../assets/testimonials/client1.jpg";
-import Client2 from "../../../assets/testimonials/client2.png";
-import Client3 from "../../../assets/testimonials/client3.jpg";
-import Client4 from "../../../assets/testimonials/client4.png";
-import Client5 from "../../../assets/testimonials/client5.jpeg";
-import Client6 from "../../../assets/testimonials/client6.jpeg";
-import Client7 from "../../../assets/testimonials/client7.jpg";
+import Client1 from "../../../assets/testimonials/clients/client1.jpg";
+import Client2 from "../../../assets/testimonials/clients/client2.png";
+import Client3 from "../../../assets/testimonials/clients/client3.jpg";
+import Client4 from "../../../assets/testimonials/clients/client4.png";
+import Client5 from "../../../assets/testimonials/clients/client5.jpeg";
+import Client6 from "../../../assets/testimonials/clients/client6.jpeg";
+import Client7 from "../../../assets/testimonials/clients/client7.jpg";
 
+import ArrowActive from "../../../assets/testimonials/miscellaneous/arrow-active.svg?react";
+import ArrowDisabled from "../../../assets/testimonials/miscellaneous/arrow-disabled.svg?react";
 
 const data = [
   {
@@ -77,60 +83,131 @@ const data = [
   }
 ];
 
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+    slidesToSlide: 1 // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 640 },
+    items: 2,
+    slidesToSlide: 1 // optional, default to 1.
+  },
+  mobile: {
+    breakpoint: { max: 640, min: 0 },
+    items: 1,
+    slidesToSlide: 1 // optional, default to 1.
+  }
+};
+
 
 export default function Testimonials() {
-  const responsive = {
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3,
-      slidesToSlide: 1 // optional, default to 1.
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2,
-      slidesToSlide: 1 // optional, default to 1.
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-      slidesToSlide: 1 // optional, default to 1.
+
+  const isMobileOrTablet = useMediaQuery('(max-width: 1024px)');
+
+  const [isDisabled, setIsDisabled] = React.useState({
+    leftArrowBtn: true,
+    rightArrowBtn: false,
+  });
+
+  const carouselRef = React.useRef(null);
+  const leftArrowBtnRef = React.useRef(null);
+  const rightArrowBtnRef = React.useRef(null);
+
+  const disableArrowBtn = React.useCallback(setIsDisabled => {
+
+    const { 
+      listRef: {
+        current : { 
+          nextElementSibling  : { 
+            firstElementChild: firstDot,
+            lastElementChild: lastDot
+          } 
+        }
+      } 
+    } = carouselRef.current;
+
+    const activeClassName = 'react-multi-carousel-dot--active';
+
+    if (firstDot.classList.contains(activeClassName)) {
+      setIsDisabled({ leftArrowBtn: true, rightArrowBtn: false });
     }
-  };
+    else if (lastDot.classList.contains(activeClassName)) {
+      setIsDisabled(prev => ({ ...prev, rightArrowBtn: true }));
+    }
+    else {
+      setIsDisabled(prev => ({ ...prev, leftArrowBtn: false }));
+    }
+  }, []);
 
   return (
-    <section className="testimonials flex flex-col items-center pb-10 mb-10">
+    <section className="testimonials flex flex-col items-center gap-10 pb-10 mb-10">
       <h1 className="text-5xl font-bold">
         Testimonials
       </h1>
 
-      <Carousel
-        responsive={responsive}
-        showDots={true}
-        infinite={true}
-        autoPlay={true}
-        keyBoardControl={true}
-        removeArrowOnDeviceType={["tablet", "mobile"]}
-        containerClass="carousel-container w-[95vw] h-80"
-      >
-        {data.map(item => (
-          <div 
-            key={item.id} 
-            className="flex flex-col justify-center items-center gap-5 p-5 mx-5 border rounded-lg"
-          >
-            <p className='text-center'>{item.text}</p>
-            <div className="avatar-details flex gap-5">
-              <Avatar 
-                src={item.avatar.profilePic}
-                alt={`${item.avatar.name} Avatar`}
-              />
-              <div className="info">
-                <div className='name'>{item.avatar.name}</div>
-                <div className='location'>{item.avatar.location}</div>
+      <div className="carousel-parent-cont">
+        <Carousel
+          ref= {carouselRef}
+          responsive={responsive}
+          showDots
+          rewind
+          rewindWithAnimation
+          autoPlay
+          keyBoardControl
+          removeArrowOnDeviceType={["tablet", "mobile"]}
+          containerClass="carousel-container w-[97vw] pb-10"
+          itemClass="sm:px-5"
+          afterChange={(...rest) => 
+            isMobileOrTablet && disableArrowBtn(setIsDisabled)
+          }
+        >
+          {data.map(item => (
+            <div 
+              key={item.id} 
+              className="flex flex-col justify-center items-center gap-5 p-5 border rounded-lg"
+            >
+              <p className='text-center'>{item.text}</p>
+              <div className="avatar-details flex gap-5">
+                <Avatar 
+                  src={item.avatar.profilePic}
+                  alt={`${item.avatar.name} Avatar`}
+                />
+                <div className="info">
+                  <div className='name'>{item.avatar.name}</div>
+                  <div className='location'>{item.avatar.location}</div>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </Carousel>
+          ))}
+        </Carousel>
+
+        <div className="carousel-arrows flex justify-between lg:hidden">
+          <button
+            ref={leftArrowBtnRef}
+            className="left-arrow-btn"
+            disabled={isDisabled.leftArrowBtn}
+            onClick={() => carouselRef.current.previous()}
+          >
+            {!isDisabled.leftArrowBtn ?
+              <ArrowActive className="left-arrow-active rotate-180"/> :
+              <ArrowDisabled className="left-arrow-disabled "/>
+            }
+          </button>
+          <button
+            ref={rightArrowBtnRef}
+            className="right-arrow-btn"
+            disabled={isDisabled.rightArrowBtn}
+            onClick={() => carouselRef.current.next()}
+          >
+            {!isDisabled.rightArrowBtn ?
+              <ArrowActive className="right-arrow-active"/> :
+              <ArrowDisabled className="right-arrow-disabled rotate-180"/>
+            }
+          </button>
+        </div>
+      </div>
     </section>
   );
 }
